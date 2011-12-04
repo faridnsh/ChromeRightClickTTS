fbbd3d823eb828a6f3ee5a9fcbd1b289f6d93d03
alFReD-NSH <FaridN_SOAD@yahoo.com>
2011-12-04 18:13:42 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+chrome.contextMenus.create({
+    title : 'speak',
+    onclick : function(info) {
+        var str = info.selectionText
+        var options = {
+            "lang" : "en-US",
+            "voiceName" : "Flite SLT American Female",
+            "rate" : 1,
+            "pitch" : 1,
+            "volume" : 1
+        }
+        options.onEvent = function(event) {
+            console.log(event);
+        };
+        chrome.tts.speak(str, options, function() {
+            if(chrome.extension.lastError) {
+                console.log('TTS Error: ' + chrome.extension.lastError.message);
+            }
+        });
+    },
+    contexts : ['selection']
+});
